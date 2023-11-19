f5baae68b631b42b4f96d66d757d6991dbc447ea
ahmet <ahmetdavresh@gmail.com>
2023-11-19 17:09:27 +0300

    fix lab 7

--- src/main/java/ru/ahmetdavresh/springthymeleafwebapp/controller/StudentController.java
+++ src/main/java/ru/ahmetdavresh/springthymeleafwebapp/controller/StudentController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import ru.ahmetdavresh.springthymeleafwebapp.dao.StudentRepository;
@@ -30,18 +31,18 @@ public class StudentController {
     public ModelAndView addStudentForm() {
         ModelAndView mav = new ModelAndView("add-student-form");
         Student student = new Student();
-        mav.addObject("students", studentRepository.findAll());
+        mav.addObject("student", student);  // Измените "students" на "student"
         return mav;
     }
 
-    @GetMapping("/saveStudent")
+    @PostMapping("/saveStudent")
     public String saveStudent(@ModelAttribute Student student) {
         studentRepository.save(student);
         return "redirect:/list";
     }
 
     @GetMapping("/showUpdateForm")
-    public ModelAndView showUpdateForm(@RequestParam Long studentId) {
+    public ModelAndView showUpdateForm(@RequestParam Integer studentId) {
         ModelAndView mav = new ModelAndView("add-student-form");
         Optional<Student> optionalStudent = studentRepository.findById(studentId);
         Student student = new Student();
@@ -53,7 +54,7 @@ public class StudentController {
     }
 
     @GetMapping("/deleteStudent")
-    public String deleteStudent(@RequestParam Long studentId) {
+    public String deleteStudent(@RequestParam Integer studentId) {
         studentRepository.deleteById(studentId);
         return "redirect:/list";
     }
--- src/main/java/ru/ahmetdavresh/springthymeleafwebapp/dao/StudentRepository.java
+++ src/main/java/ru/ahmetdavresh/springthymeleafwebapp/dao/StudentRepository.java
@@ -3,5 +3,5 @@ package ru.ahmetdavresh.springthymeleafwebapp.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.ahmetdavresh.springthymeleafwebapp.entity.Student;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
